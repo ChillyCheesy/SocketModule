f534ace9c81056fc11c44500ebb8f3ee143b1cc2
AymericHenouille <aymeric.henouille@gmail.com>
2023-01-08 15:09:42 +0100

    feat: init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Socket Module
+
+It is a Module for Modulo. 
+It facilitates the management of communications by a Sockets protocol.
---
+++ src/main/java/com/chillycheesy/socket/SocketModule.java
@@ -0,0 +1,27 @@
+package com.chillycheesy.socket;
+
+import com.chillycheesy.modulo.modules.Module;
+
+/**
+ *
+ *
+ * @author chillycheesy
+ */
+public class SocketModule extends Module {
+
+    @Override
+    protected void onLoad() {
+
+    }
+
+    @Override
+    protected void onStart() {
+
+    }
+
+    @Override
+    protected void onStop() {
+
+    }
+
+}
